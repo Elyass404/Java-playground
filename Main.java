9a198e50134135f3926f9e68d41e3df287ead337
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-21 15:55:33 +0100

    tried the type casting with the wedening casting that happens automatically

--- Main.java
+++ Main.java
@@ -4,12 +4,25 @@ public class Main{
     public static void main (String[] args){
 
         var hello = "hello"; 
-        var helloo ; //this will cause an error, because the var when used you should assigna value to the variable; 
+
+        //var helloo ; //this will cause an error, because the var when used you should assigna value to the variable; 
+       
+        // ArrayList<String> list = new ArrayList<String>();
+
+        //type casting
+        //1- wedening casting : from a smaller type to a large type
+        int intNumber = 15;
+        double doubleNumber = intNumber; 
+
+        System.out.println(doubleNumber);
+
+    
+
 
         User user = new User();
-
-        System.out.println(helloo);
-
         user.numbers();
+
+        // System.out.println(helloo);
+
     }
 }
