c1ef20d1c552119d63fcbb8379f834d3806e92b9
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-21 15:33:39 +0100

    tried the var keyword instead of type

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ public class Main{
 
     public static void main (String[] args){
 
+        var hello = "hello"; 
+        var helloo ; //this will cause an error, because the var when used you should assigna value to the variable; 
+
         User user = new User();
+
+        System.out.println(helloo);
 
         user.numbers();
     }
