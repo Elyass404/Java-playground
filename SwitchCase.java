1fb0d61f2ce59aad6676de65595caa5c2ce5d34d
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-22 00:19:05 +0100

    made a switch case example

---
+++ SwitchCase.java
@@ -0,0 +1,16 @@
+public class SwitchCase{
+    public static void main(String[] args){
+        int x = 55;
+        switch(x){
+            case 1: 
+            System.out.println("it is the number 1");
+            break;
+            case 55: 
+            System.out.println("it is the number 55");
+            break;
+            default:
+            System.out.println("it is the nummber nothing");
+            
+        }
+    }
+}
