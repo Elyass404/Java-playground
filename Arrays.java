b732c0a60448fa1885f4f464e68f27c7beb8299e
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-22 10:07:49 +0100

    played with for and for each loops in arrays, and created a simple exercice with them

--- Arrays.java
+++ Arrays.java
@@ -13,5 +13,32 @@ public class Arrays{
         //to print the length of an array
         System.out.println(clockNums.length);
 
+        //to loop through an array, we can use the .length keyword to specify the condition of the for loop
+        for (int i = 0 ; i < clockNums.length ; i++){
+            System.out.println(clockNums[i]);
+            }
+
+        //you can also loop using the for each syntaxe: 
+        for(String type : musicTypes){
+            System.out.println(type);
+        }
+
+        //exercice in the arrays and looping through it: find the lowes age among the ages
+
+        int[] ages = {14,25,62,37,12,2,52};
+        int youngest = ages[0];
+
+        for (int age : ages){
+            if (age < youngest ){
+                youngest = age;
+            }
+        }
+        System.out.println("The youngest Age in the array is: " + youngest);//result is 2
+
+
+
+
+
+
+        }
     }
-}
