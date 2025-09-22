560cfa1b3e7b1a23c56ae7fad497238f1416f792
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-22 01:08:56 +0100

    made a foreach example to loop through an array of cars as an example

---
+++ ForEachLoop.java
@@ -0,0 +1,13 @@
+public class ForEachLoop{
+    public static void main(String[] args){
+        
+        String[] cars = {"Mercedes", "BMW", "Lancia", "Mclaren"};
+
+        for(String car : cars){ //this means that each element in the array "cars" will be called a "car" and it will be string 
+            System.out.println(car);
+        }
+
+
+        HashMap<String> comptes = new HashMap<String>();
+    }
+}
