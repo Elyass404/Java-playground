5b6e1286d1be9f4cc3c8227256d36b4e9043efb5
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-25 10:09:57 +0100

    added a class of driver, and Main, then made instances of drivers and put them a list  to practice with streams

---
+++ streams/Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+
+    String name ;
+    int titles; 
+    String team; 
+
+    public Driver (String name, int titles, String team){
+        this.name  =name;
+        this.titles =titles;
+        this.team  =team;
+    }
+}
---
+++ streams/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main{
+
+    public static void main(String[] args) {
+        List<Driver> drivers = new ArrayList<>();
+
+        drivers.add(new Driver("Hamilton",8,"Mercedes"));
+        drivers.add(new Driver("Schumacher",7,"Ferrari"));
+        drivers.add(new Driver("Senna",3,"Mclaren "));
+
+        
+    }
+ 
+}
