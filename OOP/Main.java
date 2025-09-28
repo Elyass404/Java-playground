c1d5c09ebf37d3ea38d81eedba96e3905ede514c
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-28 18:12:49 +0100

    showed an example to how to access and change the attributes of an object using the dot way obj.something

--- OOP/Main.java
+++ OOP/Main.java
@@ -12,10 +12,19 @@ public class Main {
 
 
         //now we will call the method in the instance to use it in here
-        int result = secondClassObj.ShowVariable();
-        int result2 = secondClassObj2.ShowVariable();
+        int result = secondClassObj.ShowVariableByTwo();
+        int result2 = secondClassObj2.ShowVariableByTwo();
 
-        System.out.println(result);
-        System.out.println(result2);
+        //we can also access the attributes of an object directly without using a method that returns them by using the dot "." way
+        System.out.println(secondClassObj.variable); //will show the 31
+
+        //and we can change the attributes also (lets change the 31 to 41 in the secondClassObj2)
+        secondClassObj2.variable = 41;
+        System.out.println(secondClassObj2.variable); //channged to 41
+        int newValOfVariable = secondClassObj2.ShowVariableByTwo(); 
+        System.out.println(newValOfVariable); // will show 82 because the variable set before in the obj is changed and will affect all the methods inside it that uses the variable)
+
+        System.out.println(result); //will show 62
+        System.out.println(result2); //will show 62
     }
 }
--- OOP/SecondClass.java
+++ OOP/SecondClass.java
@@ -2,8 +2,8 @@ public class SecondClass{
 
     public int variable   = 31;
 
-    public int ShowVariable(){
-        return variable;
+    public int ShowVariableByTwo(){
+        return variable * 2;
     }
-    
+
 }
