75f19d5f11dc162b32bb34d6ea398609e90047ec
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-09-28 17:58:39 +0100

    showed that we can make multiple objects from the same class

--- OOP/Main.java
+++ OOP/Main.java
@@ -5,11 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
         // we created an instance of the class "Secondclass"
-        SecondClass secondClass = new SecondClass();
+        SecondClass secondClassObj = new SecondClass();
+
+        //We can make multiple objects from the same class 
+        SecondClass secondClassObj2 = new SecondClass();
+
 
         //now we will call the method in the instance to use it in here
-        int result = secondClass.ShowVariable();
-        
+        int result = secondClassObj.ShowVariable();
+        int result2 = secondClassObj2.ShowVariable();
+
         System.out.println(result);
+        System.out.println(result2);
     }
 }
