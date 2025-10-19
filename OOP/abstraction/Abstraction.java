9022f95c008d6325dc79fa2eecb33d7eb9af640e
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-19 15:06:30 +0100

    explained what is abstraction, and what is used for, and gave some notes to understand the abstraction well

--- OOP/abstraction/Abstraction.java
+++ OOP/abstraction/Abstraction.java
@@ -1,18 +1,30 @@
+//Abstraction is the concepts of hiding things and show only the important ones, without distressing others with the content of the of methods etc 
+
+//To apply the abstraction you should use the keyword of "abstract" in the class and/or in the method
+
+/*An abstract method, is a method that is made inside an abstract class, and  it has only the name (signature) without defining its body
+and when another class is inheriting the abstract class it is obligatory for the subclass to redefine that abstact method (method without body in the parent abstract class)
+or that would cause an error */
+
+/* Important NOTE: To make an abstract method you should make its class abstract, you can not make an abstract method inside a normal class
+but in the other hand you can make an abstract class without any abstract method if you want */
+
 abstract class Car{
     protected String headLinghts = "LED";
     protected String engineType ;
 
-    public abstract void showEngineType();
+    public abstract void showEngineType(); //this method should be redefined once another class inherites this abstract class
 
     public void showHeadLight(){
-        System.out.println("the headlights in this is: " + this.headLinghts);
+        System.out.println("the headlights in this is: " + this.headLinghts); // an abstract class can have also normal method that have body
     }
 }
 
 class Mercedes extends Car{
 
-    public void showEngineType(){
+    public void showEngineType(){ // redefining the abstract method that exists in the abstract parent class
         this.engineType = "petrol";
+        // we can do whatever we want this method to do; 
         System.out.println("the engine type of this car is: " + this.engineType);
 
     }
@@ -21,8 +33,9 @@ class Mercedes extends Car{
 
 class Bmw extends Car{
 
-    public void showEngineType(){
+    public void showEngineType(){ // redefining the abstract method that exists in the abstract parent class
         this.engineType = "Deisel";
+        //we can do wahtever we want this method to do; 
         System.out.println("the engine type of this car is: " + this.engineType);
 
     }
@@ -34,6 +47,7 @@ class Abstraction{
         Car merc = new Mercedes();
         Car bmw = new Bmw();
 
+        //now and even the two classes have the same method names, eachone of them is doing something different because we redefined them
         merc.showEngineType();
         bmw.showEngineType();
 
