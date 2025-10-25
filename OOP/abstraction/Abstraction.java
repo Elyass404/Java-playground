a6c22d065f90818995c5cf2ac0b84023b1f795f2
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-25 23:30:16 +0100

    added a folder of interface to explain the interfac, and also created an interface file that contains three methods

--- OOP/abstraction/Abstraction.java
+++ OOP/abstraction/Abstraction.java
@@ -1,6 +1,6 @@
 //Abstraction is the concepts of hiding things and show only the important ones, without distressing others with the content of the of methods etc 
 
-//To apply the abstraction you should use the keyword of "abstract" in the class and/or in the method
+//To apply the abstraction you should use the keyword of "abstract" in the class and/or in the method and you cant instanciate from it
 
 /*An abstract method, is a method that is made inside an abstract class, and  it has only the name (signature) without defining its body
 and when another class is inheriting the abstract class it is obligatory for the subclass to redefine that abstact method (method without body in the parent abstract class)
---
+++ OOP/interfaceExplaination/Interface.java
@@ -0,0 +1,6 @@
+class Interfae{
+
+    void methodOne();
+    Int methodTwo(int number); // this method accepts one argument 
+    String methodThree(String fname , lname); // this method accepts two arguments
+}
