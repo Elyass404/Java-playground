fa1f10d336aad615f607f0c276af02446c76f403
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-25 23:56:41 +0100

    started explainign what is the interface

--- OOP/interfaceExplaination/Interface.java
+++ OOP/interfaceExplaination/Interface.java
@@ -1,5 +1,9 @@
  interface Interface{
 
+    //an interface is like an abstaract class, like we saw before, you cant instanciate from it and it has methods
+    //that should be redefined by the the child class (we call them implimanting classes because they implement the interface)
+    //but the deffirence here is that the interfaces would never ever have bodies in their declared mehtods
+
     void methodOne();
     int methodTwo(int number); // this method accepts one argument 
     String methodThree(String fname , String lname); // this method accepts two arguments
