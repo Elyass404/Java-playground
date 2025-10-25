90f192d2d127632868e41ab79f44f7a15d60152c
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-26 00:00:02 +0100

    expained more what is the interface and what is used for

--- OOP/interfaceExplaination/Interface.java
+++ OOP/interfaceExplaination/Interface.java
@@ -4,6 +4,9 @@
     //that should be redefined by the the child class (we call them implimanting classes because they implement the interface)
     //but the deffirence here is that the interfaces would never ever have bodies in their declared mehtods
 
+    //because the interface plays like a contract that a developer should respect, so he should redefine all the methods from the implimented
+    //interface or that will cause error, and with that the developer doesnt miss any method
+
     void methodOne();
     int methodTwo(int number); // this method accepts one argument 
     String methodThree(String fname , String lname); // this method accepts two arguments
