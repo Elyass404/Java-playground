9f63cb16e9fa742d74456116d58d34ea12338b7a
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-25 23:34:07 +0100

    created a class that impliments the 'interface' interface so i can continue with the explination

---
+++ OOP/interfaceExplaination/InterfaceImplimentation.java
@@ -0,0 +1,10 @@
+class InterfaceImplimentation implements Interfae{
+
+    public String fname = "ilyass";
+    public String fname = "developer";
+    public int number = 3100;
+
+    public void methodOne(){
+        System.out.println("this is the first method implimented!");
+    }
+}
