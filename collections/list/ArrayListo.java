cd7a4ee4fb60ae18b5dbfcb2c845a5c6a6cebc0f
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-10-28 23:25:47 +0100

    added a folder for the collection, and started with the array list making a simple example fo the test

---
+++ collections/list/ArrayListo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList; // Import the ArrayList class
+
+class ArrayListo{
+
+
+
+    public static void main (String[] args){
+
+    ArrayList<String> arrayList = new ArrayList <String>();
+
+    arrayList.add("helllo");
+
+        System.out.println(arrayList.get(0));
+    }
+}
