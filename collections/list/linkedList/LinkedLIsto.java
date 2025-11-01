345f045b46f6169b3c6c6bbee7e27e16653830ce
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-01 23:44:22 +0100

    added a file for the linked list, and explained what is the difference between it and array list, and gave the additional method that the linkedlist has and the arraylist doesnt

---
+++ collections/list/linkedList/LinkedLIsto.java
@@ -0,0 +1,27 @@
+/*LinkedList: as we saw in the ArrayList, LinkedList also has the same methods as well like the ones in the array list , because both 
+are under the interface of the List, but both work are built differently, you can search of the building differences between them both
+if you want to get morre details. but here we will disscuss which one should we use and when */
+
+/*When to use that and this: 
+So, the as we said before, both are the same(kind of) and you can add,get, set,...etc  data using the same methods, but LinkedList has
+additional method that can help us manipulate the data inside the collection, like get first and add first , or last etc , so it is more flexible 
+when it comes to playing with the data inside the collection, we will see  the application of linked list below*/
+
+/* The different additional method of the LinkedList: */
+/* addFirst() */
+/* addLast() */
+/* removeFirst() */
+/* removeLast() */
+/* getFirst() */
+/* getLast() */
+
+Import java.util.List
+Import java.util.LinkedList
+
+public class LinkedListo{
+
+public static void main(){
+    
+}
+
+}
