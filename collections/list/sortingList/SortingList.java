d182f209160d760c30958b00c07d167d3cbe731b
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-17 23:57:43 +0100

    applied the reversed sorting using the reverseOrder method

--- collections/list/sortingList/SortingList.java
+++ collections/list/sortingList/SortingList.java
@@ -15,7 +15,12 @@ carsList.add("Lacia");
 
 Collections.sort(carsList);
 
+// create a copy and sort the copy in reverse order (Collections.sort returns void)
+List<String> reversedList = new ArrayList<>(carsList);
+Collections.sort(reversedList, Collections.reverseOrder());
+
 System.out.println(carsList);
+System.out.println(reversedList);
 
 }
 
