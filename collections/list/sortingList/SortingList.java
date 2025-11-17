3cf0aca5ca7554b30bde355a8598fbc1be106adb
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-17 23:49:20 +0100

    added a file to demostrate the sorting of the list in the collections class provided by java

---
+++ collections/list/sortingList/SortingList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingList {
+
+public static void main (String[] args){
+
+List<String> carsList = new ArrayList<>();
+carsList.add("Mercedes");
+carsList.add("Dacia ostora");
+carsList.add("BYD");
+carsList.add("Audi");
+carsList.add("Lacia");
+
+System.out.println(carsList);
+
+}
+
+}
