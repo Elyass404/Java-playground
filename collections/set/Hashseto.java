87b0bbb8161f6fbe778e7fbb6d9239af617e6b70
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-21 23:41:56 +0100

    added a set of elements and used the mehtod of add and added elements in the set

--- collections/set/Hashseto.java
+++ collections/set/Hashseto.java
@@ -2,7 +2,17 @@ import java.util.HashSet; //import the hashset
 
 public class Hashseto{
 
+public static void main (String[] args){
+
 HashSet<String> theSet = new HashSet<String>();
+
+theSet.add("element one");
+theSet.add("element two");
+theSet.add("element three");
+theSet.add("element four");
+
+System.out.println(theSet);
+}
 
 
 
