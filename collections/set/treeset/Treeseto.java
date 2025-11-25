788e648ceccaaf4fd907002d3d3298913e246dcc
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-25 23:47:53 +0100

    added the comment explaining the differece between the hashset and the treeset

--- collections/set/treeset/Treeseto.java
+++ collections/set/treeset/Treeseto.java
@@ -1,6 +1,9 @@
 import java.util.TreeSet; 
 import java.util.Set; 
 
+//The treeset is like the hashsetbut the difference happens in the sorting of the elements inside the treeset
+//the treeset automatically sorts the elements even if we entered them not sorted
+
 public class Treeseto{
 
     public static void main(String[] args){
