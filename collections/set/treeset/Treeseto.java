a65537691d63ad5970e769cb474998d985143188
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-11-25 23:45:54 +0100

    added a file to explain the tree set and implemented it

---
+++ collections/set/treeset/Treeseto.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet; 
+import java.util.Set; 
+
+public class Treeseto{
+
+    public static void main(String[] args){
+
+        Set<String> theTree = new TreeSet<>();
+
+        theTree.add("elementOne");
+
+
+        System.out.println(theTree);
+    
+    }
+}
