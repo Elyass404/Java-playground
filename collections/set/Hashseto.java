c2bac265feadbc603d6b98e2f5f1a351bf8e5b8a
ilyass mar <ilyass.marghine22@gmail.com>
2025-12-20 23:35:29 +0100

    just a comment

--- collections/set/Hashseto.java
+++ collections/set/Hashseto.java
@@ -26,6 +26,8 @@ System.out.println(size);
 //to remove all the elements from the set at once,
 //theSet.clear(); // uncomment this line in order to execute the method     
 
+//just a comment
+
 //to loop through a hasshset
 for (String element : theSet ){
     System.out.println (element);
