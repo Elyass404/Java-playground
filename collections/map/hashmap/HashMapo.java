c5b5e89ad9d9c750f05d2d505c5cd87012647ebe
ilyass mar <ilyass.marghine22@gmail.com>
2025-12-30 23:58:37 +0100

    added a comment explaining hashmap a little, and will make examples about it in the same file later

--- collections/map/hashmap/HashMapo.java
+++ collections/map/hashmap/HashMapo.java
@@ -1,6 +1,23 @@
 package collections.map.hashmap;
 
+//A HashMap stores items in key/value pairs, where each key maps to a specific value.
+/*
+
+
+It is part of the java.util package and implements the Map interface.
+
+Instead of accessing elements by an index (like with ArrayList), you use a key to retrieve its associated value.
+
+A HashMap can store many different combinations, such as:
+
+String keys and Integer values
+String keys and String values
+
+*/
+
 public class HashMapo {
-  
+
+
+
   
 }
