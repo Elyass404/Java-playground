929193a7ede0768cb4a81e152e13415e43cf9f4c
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-03 21:45:36 +0100

    added a comment expalainign the what is the tree map and its specifications, and also, added the way how to get the elements from the treemap using the mehthod get()

--- collections/map/hashmap/TreeMapo.java
+++ collections/map/hashmap/TreeMapo.java
@@ -2,6 +2,8 @@ package collections.map.hashmap;
 import java.util.Map;
 import java.util.TreeMap;
 
+//A TreeMap stores items in key/value pairs in a sorted order based on the keys. so it does care about the sorting of the keys
+
 public class TreeMapo {
 
   public static void main(String[] args) {
@@ -13,9 +15,13 @@ public class TreeMapo {
     treeMap.put("carOne", "Mercedes");
     treeMap.put("carTwo", "BMW");
     treeMap.put("carThree", "Dacia");
-    treeMap.put("carThree", "Audi");
-    treeMap.put("carThree", "Audi");
+    treeMap.put("carFour", "Audi");
+    treeMap.put("carFour", "Audi"); //this will not be added because the key "carThree" already exists, so the value will be updated to "Audi"
 
+    //to get an elemement from the treeMap collection we use the method get(), with the name of the key not the index
+    System.out.println(treeMap.get("carThree"));
+
+    System.out.println(treeMap);
 
   }
   
