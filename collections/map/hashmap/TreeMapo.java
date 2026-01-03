61226e497f181a5b794164e63b537957d045b4b5
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-03 21:40:05 +0100

    added a file for the tree map and inserted some elements in it using the put method

---
+++ collections/map/hashmap/TreeMapo.java
@@ -0,0 +1,22 @@
+package collections.map.hashmap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapo {
+
+  public static void main(String[] args) {
+  
+    Map<String, String> treeMap = new TreeMap<>();
+
+    //to add elemetns to the TreeMap we use the method put()
+    
+    treeMap.put("carOne", "Mercedes");
+    treeMap.put("carTwo", "BMW");
+    treeMap.put("carThree", "Dacia");
+    treeMap.put("carThree", "Audi");
+    treeMap.put("carThree", "Audi");
+
+
+  }
+  
+}
