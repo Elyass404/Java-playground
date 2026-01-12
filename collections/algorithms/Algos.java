520a64b31e135aafb5369e24e69b53ce995b50cd
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-12 23:49:16 +0100

    added the method of binary search example

--- collections/algorithms/Algos.java
+++ collections/algorithms/Algos.java
@@ -1,6 +1,7 @@
 package collections.algorithms;
 
 import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class Algos {
@@ -26,7 +27,18 @@ public class Algos {
     numbers.add(12);
     numbers.add(1);
 
+    //to search in certain collection, we can use the Collection.binarySearch().NOTE: the collection should be sorted first using sort() method
+
+    Collections.sort(cars); //sorted first
     
+    //now lets search for Mercedes and print it
+    String myCar = cars.get(Collections.binarySearch(cars, "Mercedes")); // here the binary search get the index of the element we are looking for that is why i used it inside the get method
+    System.out.println("my dream car is: "+ myCar);
+
+    //
+
+
+
 
   }
 }
