65ccc220ac5d7d9766eb08ffc885cdf8205755cb
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-12 23:31:40 +0100

    created a file to make the algorithms, and use them to show their utility, and created two list that will be our collections to try the algorithms on

---
+++ collections/algorithms/Algos.java
@@ -0,0 +1,32 @@
+package collections.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algos {
+  
+
+  public static void main(String[] args) {
+    
+    //To use the java provided algorithms for collections, we should first make a certain collection to play with 
+    List<String> cars = new ArrayList<>();
+    List<Integer> numbers = new ArrayList<>();
+
+    //add elements to the cars list
+    cars.add("Ford");
+    cars.add("Mercedes");
+    cars.add("BMW");
+    cars.add("Dacia the lengend");
+    cars.add("Maseratti");
+
+    //add elements to the numbers list 
+    numbers.add(25);
+    numbers.add(85);
+    numbers.add(125);
+    numbers.add(12);
+    numbers.add(1);
+
+    
+
+  }
+}
