04816f2588336d32368a105c5696fd6908cf6b22
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-12 23:18:34 +0100

    added a comment defining what is iterator and its utility

--- collections/iterator/Iteratora.java
+++ collections/iterator/Iteratora.java
@@ -1,5 +1,6 @@
-
-
+//An Iterator is an object that can be used to loop through collections, like ArrayList and HashSet.
+// It is called an "iterator" because "iterating" is the technical term for looping.
+// To use an Iterator, you must import it from the java.util package.
 package collections.iterator;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ class Iteratora {
     System.out.println(it.next());
 
     while (it.hasNext()) {
-      System.out.println("still have element");
+      System.out.println("still have element"); // infinite loop be aware
     }
 
   }
