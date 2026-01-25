0cce9f88579cffb1a59c73e5f9b4405e4cab890b
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-25 23:44:45 +0100

    added a folder for wrapper classes and gave a small description of what are wrapper classes

---
+++ wrapperClasses/WrapperClasses.java
@@ -0,0 +1,6 @@
+package wrapperClasses;
+//wrapper classes, are classes (obviously hahah) tha provide a way to use primitive data as objects, so we can be able to use pre existing methods with the variables, 
+
+public class WrapperClasses {
+  
+}
