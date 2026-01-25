bcd4648a4534bf18234b32c9ba25a69ad5e4e00e
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-25 23:49:29 +0100

    added the different wrapper classes and assign variables to them,  in order to be able to make examples using them

--- wrapperClasses/WrapperClasses.java
+++ wrapperClasses/WrapperClasses.java
@@ -3,4 +3,15 @@ package wrapperClasses;
 
 public class WrapperClasses {
   
+  public static void main(String[] args) {
+    // here are the different wrapper classes 
+    Integer number =  15;
+    Long longNumber= 112L;
+    Double doubleNumber = 12.25;
+    Float floatNumber = 1.21f ; 
+    Character charac = 'A';
+    System.out.println(number);
+    System.out.println(doubleNumber);
+    System.out.println(charac);
+  }
 }
